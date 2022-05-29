ce072dd7013d511429c2c6cea61e2a1de073b263
paras-zomby <765143903@qq.com>
2022-05-29 15:23:03 +0800

    fix: uncorrect threshold control

--- cluster.py
+++ cluster.py
@@ -13,10 +13,10 @@ if __name__ == '__main__':
     MAX_SIZE = 600000
     # cluster = KMeans(n_clusters=2, init='k-means++')
     cluster = KMeans(num_cluster=2)
-    # cluster = DBSCAN(eps=100)
 
     for img_path in pics:
         img = cv.imread(img_path)
+        # 防止图片过大
         if np.prod(img.shape[:2]) > MAX_SIZE:
             ratio = MAX_SIZE / np.prod(img.shape[:2])
             img = cv.resize(img, None, None, ratio, ratio)
@@ -25,10 +25,9 @@ if __name__ == '__main__':
         cluster.fit(img_f[np.random.randint(0, img_f.shape[0], size=int(img_f.shape[0] * 0.7))])
         result = cluster.predict(img_f)
         mask = result.reshape(img.shape[:2]).astype(bool)
-        print(f"sum class 1 = {mask.sum()}, sum class 2 = {(~mask).sum()}")
-        # img[mask] = np.array([0, 0, 255])
+        print(f"class 1 pixels = {mask.sum()}, class 2 pixels = {(~mask).sum()}")
         cv.imshow("raw img", img)
-        img[mask] = cv.addWeighted(img[mask], 0.7, np.zeros_like(img[mask]) + np.array([0, 0, 255]), 0.3, 0, dtype=cv.CV_8UC3)
-        img[~mask] = cv.addWeighted(img[~mask], 0.7, np.zeros_like(img[~mask]) + np.array([0, 255, 0]), 0.3, 0, dtype=cv.CV_8UC3)
+        img[mask] = cv.addWeighted(img[mask], 0.7, np.zeros_like(img[mask]) + np.array([0, 0, 255]), 0.35, 0, dtype=cv.CV_8UC3)
+        img[~mask] = cv.addWeighted(img[~mask], 0.7, np.zeros_like(img[~mask]) + np.array([0, 255, 0]), 0.35, 0, dtype=cv.CV_8UC3)
         cv.imshow("cls img", img)
         cv.waitKey(0)
--- kmeans.py
+++ kmeans.py
@@ -2,22 +2,27 @@ import numpy as np
 
 
 class KMeans(object):
-    def __init__(self, num_cluster):
+    def __init__(self, num_cluster, threshold=1e-4):
         super().__init__()
         self.num_cluster = num_cluster
+        self.threshold = threshold
         self.centers = [None] * num_cluster  # [None, None]
 
     def fit(self, x: np.ndarray):
-        cluster_center = x[np.random.choice(range(len(x)), size=self.num_cluster, replace=False)]
+        cluster_center = x[np.random.choice(range(len(x)), size=self.num_cluster, replace=False)].copy()
         last_center = np.zeros_like(cluster_center)
         cluster = np.zeros(x.shape[0], dtype=int)
-        while max([self.calc_dis(x, y) for x, y in zip(cluster_center, last_center)]) > 0.00001:
-            last_center = cluster_center
+        cnt = 0
+        while max([self.calc_dis(x, y) for x, y in zip(cluster_center, last_center)]) > self.threshold:
+            print(f"INFO: epoch = {cnt}, distance change = {max([self.calc_dis(x, y) for x, y in zip(cluster_center, last_center)]):9.8f}")
+            cnt += 1
+            last_center = cluster_center.copy()
             for idx, item in enumerate(x):
                 min_idx = np.argmin([self.calc_dis(item, y) for y in cluster_center])
                 cluster[idx] = min_idx
             for idx in range(self.num_cluster):
                 cluster_center[idx] = np.mean(x[cluster == idx], axis=0)
+        print(f"INFO: epoch = {cnt}, distance change = {max([self.calc_dis(x, y) for x, y in zip(cluster_center, last_center)]):9.8f} [FINAL]")
         self.centers = cluster_center
 
     def predict(self, x: np.ndarray):
